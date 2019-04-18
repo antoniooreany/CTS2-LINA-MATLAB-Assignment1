2bad8b82378102a826da04673c6c53c2bed5dc09
antoniooreany <antonio_oreany@mail.ru>
2019-04-18 22:30:15 +0200

    second commit

---
+++ assignment1.m
@@ -0,0 +1,10 @@
+function [outputArg1,outputArg2] = assignment1(inputArg1,inputArg2)
+%UNTITLED8 Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+
+
+
+end
+
