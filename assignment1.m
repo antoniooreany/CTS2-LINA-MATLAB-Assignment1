db195f901733e6c31da874e5756010c8b93a4da1
antoniooreany <antonio_oreany@mail.ru>
2019-04-20 21:32:36 +0200

    eg assignment1(1,1) gives the result

--- assignment1.m
+++ assignment1.m
@@ -4,6 +4,51 @@ function [outputArg1,outputArg2] = assignment1(inputArg1,inputArg2)
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+rootPath = 'C:\Program Files\MATLAB\R2019a\bin\Assignments_CTS2_Gorshkov_Anton\1\';
+
+srcFileName = strcat(rootPath,'src.txt');
+
+srcFileID = fopen(srcFileName,'r');
+
+xSrcStr = fgetl(srcFileID);
+ySrcStr = fgetl(srcFileID);
+xSrcNum = str2num(xSrcStr);
+ySrcNum = str2num(ySrcStr);
+fclose(srcFileID);
+
+plot(xSrcNum, ySrcNum);
+
+complexSrc = complex(xSrcNum,ySrcNum);
+
+disp('Press any key for transformation')  % Press a key here.You can see the message 'Paused: Press any key' in        % the lower left corner of MATLAB window.
+pause;
+
+transformationMagnitude = 2;
+transformationAngleDeg = 45;
+transformationAngleRad = deg2rad(transformationAngleDeg);
+transformationComlpex = transformationMagnitude*exp(transformationAngleRad*1i);
+
+complexDst = complexSrc * transformationComlpex;
+xDstNum = real(complexDst);
+yDstNum = imag(complexDst);
+
+plot(xDstNum, yDstNum);
+
+dstFileName = strcat(rootPath,'dst.txt');
+
+dstFileID = fopen(dstFileName, 'w');
+
+xDstStr = num2str(xDstNum);
+yDstStr = num2str(yDstNum);
+
+dstData = [xDstNum; yDstNum];
+
+fprintf(dstFileID,'%6s %12s\n','xDstStr','yDstStr');
+fprintf(dstFileID,'%6.2f %12.8f\n',dstData);
+fclose(dstFileID);
+
 
 
 end
