d62efd3e1eb9ba52fbaeff3f5058a54991506cdc
antoniooreany <antonio_oreany@mail.ru>
2019-04-26 18:58:35 +0200

    the code were cleaned up.

--- assignment1.m
+++ assignment1.m
@@ -1,4 +1,4 @@
-function [] = assignment1()
+function [] = assignment1(varargin)
 
 % Write a MATLAB-function, that rotates an object O in the x-y-coordinate system of R2 by an
 % angle alpha and stretches or shrinks it by a factor lambda. Both alpha and lambda are parameters of your function.
@@ -20,57 +20,51 @@ function [] = assignment1()
 % -7 2 1 8 1 2 -7 -7 -2 -2 -7 -7
 % This object O needs to be rotated counter clockwise by alpha = 45 degrees and stretched by lambda = 2.
 
+%%%%% Setting default values in case when no parameters are passed %%%%%%%%%%%%%%%%%%%%%
+if nargin < 2   
+    srcFileName = 'src.txt';                                                    % set default src file path
+    dstFileName = 'dst.txt';                                                    % set default dst file path
+else
+    srcFileName = varargin{1};                                                  % set src file path from the function parameters
+    dstFileName = varargin{2};                                                  % set src file path from the function parameters
+end
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+srcFileID = fopen(srcFileName,'r');                                             % Open the src file for reading
+xSrcStr = fgetl(srcFileID);                                                     % Read the X-coordinates from the src file as a string
+ySrcStr = fgetl(srcFileID);                                                     % Read the Y-coordinates from the src file as a string
+fclose(srcFileID);                                                              % Close the src file
+xSrcNum = str2num(xSrcStr);                                                     % Convert X-coordinates into numbers
+ySrcNum = str2num(ySrcStr);                                                     % Convert Y-coordinates into numbers
 
-% hint: parameter rootPath = 'C:\Program Files\MATLAB\R2019a\bin\Assignments_CTS2_Gorshkov_Anton\1\';
+complexSrc = complex(xSrcNum,ySrcNum);                                          % Create a src-complex array for the next transformation
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+plot(complexSrc);                                                               % Draw the src-object
 
-% srcFileName = strcat(rootPath,'src.txt');           % Initialize the full src file name
-srcFileName = 'src.txt';                            % Initialize the full src file name
+disp('Press any key for transformation')                                        % Displaying 'Press any key for transformation'
+pause;                                                                          % Wait for the pressing any key. Meanwhile we can see to the untransformed object
 
-% TODO: Delete redundant "+"ses in the src-file. Does it still work? If
-% don't, modify the function to work with this kind of data
-
-srcFileID = fopen(srcFileName,'r');                 % Open the src file for reading
-
-xSrcStr = fgetl(srcFileID);                         % Read the X-coordinates from the src file as a string
-ySrcStr = fgetl(srcFileID);                         % Read the Y-coordinates from the src file as a string
-fclose(srcFileID);                                  % Close the src file
-xSrcNum = str2num(xSrcStr);                         % Convert X-coordinates into numbers
-ySrcNum = str2num(ySrcStr);                         % Convert Y-coordinates into numbers
-
-complexSrc = complex(xSrcNum,ySrcNum);              % Create a src-complex array for the next transformation
-
-plot(complexSrc);                                   % Draw the src-object
-
-disp('Press any key for transformation')            % Displaying 'Press any key for transformation'
-pause;                                              % Wait for the pressing any key. Meanwhile we can see to the untransformed object
-
-transformationMagnitude = 2;                                    % Transformation magnitude initialization
-transformationAngleDeg = 45;                                    % Transformation angle in degree initialization
-transformationAngleRad = deg2rad(transformationAngleDeg);       % Transformation angle in degree into radians convertion
+transformationMagnitude = 2;                                                    % Transformation magnitude initialization
+transformationAngleDeg = 45;                                                    % Transformation angle in degree initialization
+transformationAngleRad = deg2rad(transformationAngleDeg);                       % Transformation angle in degree into radians convertion
 transformationComlpex = transformationMagnitude*exp(transformationAngleRad*1i); % Transformation comlpex initialization
 
-complexDst = complexSrc * transformationComlpex;    % Applying transformation into src-object using src-complex and transformation complex.
+complexDst = complexSrc * transformationComlpex;                                % Applying transformation into src-object using src-complex and transformation complex.
 xDstNum = real(complexDst);
 yDstNum = imag(complexDst);
 
-hold on;            % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
-plot(complexDst);                                   % Draw the dst-object
+hold on;                                                                        % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
+plot(complexDst);                                                               % Draw the dst-object
+axis equal;                                                                     % use equal data unit lengths along each axis.
 
-% dstFileName = strcat(rootPath,'dst.txt');           % Initialize the full src file name
-dstFileName = 'dst.txt';                            % Initialize the full src file name
-dstFileID = fopen(dstFileName, 'w');                % Open the src file for reading
-dstData = [xDstNum; yDstNum];                       % Create dst-coordinates matrix
+dstFileID = fopen(dstFileName, 'w');                                            % Open the src file for reading
+dstData = [xDstNum; yDstNum];                                                   % Create dst-coordinates matrix
 
-% TODO: Format the table in the dst-file
+fprintf(dstFileID,'%8s               %8s\n','xDstStr','yDstStr');               % Write the header into dst-file
+fprintf(dstFileID,'%6.6f              %6.6f\n',dstData);                        % Write the coordinates into dst-file
+fclose(dstFileID);                                                              % Close dst-file
 
-fprintf(dstFileID,'%12s %12s\n','xDstStr','yDstStr');   % Write the header into dst-file
-fprintf(dstFileID,'%6.6f %6.6f\n',dstData);             % Write the coordinates into dst-file
-fclose(dstFileID);                                  % Close dst-file
-
-hold off;       % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
+hold off;                                                                       % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
 
 end
 
