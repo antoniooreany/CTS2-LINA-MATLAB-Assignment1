7a18e585165b76857827504452809c8a3be6c8b3
antoniooreany <antonio_oreany@mail.ru>
2019-04-26 20:36:36 +0200

    the long-length variables were shortened, the doc was moved into the very top.

--- assignment1.m
+++ assignment1.m
@@ -1,4 +1,3 @@
-function [] = assignment1(varargin)
 
 % Write a MATLAB-function, that rotates an object O in the x-y-coordinate system of R2 by an
 % angle alpha and stretches or shrinks it by a factor lambda. Both alpha and lambda are parameters of your function.
@@ -20,51 +19,54 @@ function [] = assignment1(varargin)
 % -7 2 1 8 1 2 -7 -7 -2 -2 -7 -7
 % This object O needs to be rotated counter clockwise by alpha = 45 degrees and stretched by lambda = 2.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [] = assignment1(varargin)
 %%%%% Setting default values in case when no parameters are passed %%%%%%%%%%%%%%%%%%%%%
-if nargin < 2   
-    srcFileName = 'src.txt';                                                    % set default src file path
-    dstFileName = 'dst.txt';                                                    % set default dst file path
+if nargin < 2
+    srcFileName = 'src.txt';                                       % set default src file path
+    dstFileName = 'dst.txt';                                       % set default dst file path
 else
-    srcFileName = varargin{1};                                                  % set src file path from the function parameters
-    dstFileName = varargin{2};                                                  % set src file path from the function parameters
+    srcFileName = varargin{1};                                     % set src file path from the function parameters
+    dstFileName = varargin{2};                                     % set src file path from the function parameters
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-srcFileID = fopen(srcFileName,'r');                                             % Open the src file for reading
-xSrcStr = fgetl(srcFileID);                                                     % Read the X-coordinates from the src file as a string
-ySrcStr = fgetl(srcFileID);                                                     % Read the Y-coordinates from the src file as a string
-fclose(srcFileID);                                                              % Close the src file
-xSrcNum = str2num(xSrcStr);                                                     % Convert X-coordinates into numbers
-ySrcNum = str2num(ySrcStr);                                                     % Convert Y-coordinates into numbers
+srcFileID = fopen(srcFileName,'r');                                % Open the src file for reading
+xSrcStr = fgetl(srcFileID);                                        % Read the X-coordinates from the src file as a string
+ySrcStr = fgetl(srcFileID);                                        % Read the Y-coordinates from the src file as a string
+fclose(srcFileID);                                                 % Close the src file
+xSrcNum = str2num(xSrcStr);                                        % Convert X-coordinates into numbers
+ySrcNum = str2num(ySrcStr);                                        % Convert Y-coordinates into numbers
 
-complexSrc = complex(xSrcNum,ySrcNum);                                          % Create a src-complex array for the next transformation
+complexSrc = complex(xSrcNum,ySrcNum);                             % Create a src-complex array for the next transformation
 
-plot(complexSrc);                                                               % Draw the src-object
+plot(complexSrc);                                                  % Draw the src-object
 
-disp('Press any key for transformation')                                        % Displaying 'Press any key for transformation'
-pause;                                                                          % Wait for the pressing any key. Meanwhile we can see to the untransformed object
+disp('Press any key for transformation')                           % Displaying 'Press any key for transformation'
+pause;                                                             % Wait for the pressing any key. Meanwhile we can see to the untransformed object
 
-transformationMagnitude = 2;                                                    % Transformation magnitude initialization
-transformationAngleDeg = 45;                                                    % Transformation angle in degree initialization
-transformationAngleRad = deg2rad(transformationAngleDeg);                       % Transformation angle in degree into radians convertion
-transformationComlpex = transformationMagnitude*exp(transformationAngleRad*1i); % Transformation comlpex initialization
+transfMagn = 2;                                                    % Transformation magnitude initialization
+transfAngleDeg = 45;                                               % Transformation angle in degree initialization
+transfAngleRad = deg2rad(transfAngleDeg);                          % Transformation angle in degree into radians convertion
+transfComlpex = transfMagn*exp(transfAngleRad*1i);                 % Transformation comlpex initialization
 
-complexDst = complexSrc * transformationComlpex;                                % Applying transformation into src-object using src-complex and transformation complex.
-xDstNum = real(complexDst);
-yDstNum = imag(complexDst);
+complexDst = complexSrc * transfComlpex;                           % Applying transformation into src-object using src-complex and transformation complex.
+xDstNum = real(complexDst);                                        % Get an array of real parts of the transformed object
+yDstNum = imag(complexDst);                                        % Get an array of imeginary parts of the transformed object
 
-hold on;                                                                        % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
-plot(complexDst);                                                               % Draw the dst-object
-axis equal;                                                                     % use equal data unit lengths along each axis.
+hold on;                                                           % retain plots in the current axes so that new plots added to the axes do not delete existing plots.
+plot(complexDst);                                                  % Draw the dst-object
+axis equal;                                                        % use equal data unit lengths along each axis.
 
-dstFileID = fopen(dstFileName, 'w');                                            % Open the src file for reading
-dstData = [xDstNum; yDstNum];                                                   % Create dst-coordinates matrix
+dstFileID = fopen(dstFileName, 'w');                               % Open the src file for reading
+dstData = [xDstNum; yDstNum];                                      % Create dst-coordinates matrix
 
-fprintf(dstFileID,'%8s               %8s\n','xDstStr','yDstStr');               % Write the header into dst-file
-fprintf(dstFileID,'%6.6f              %6.6f\n',dstData);                        % Write the coordinates into dst-file
-fclose(dstFileID);                                                              % Close dst-file
+fprintf(dstFileID,'%8s               %8s\n','xDstStr','yDstStr');  % Write the header into dst-file
+fprintf(dstFileID,'%6.6f              %6.6f\n',dstData);           % Write the coordinates into dst-file
+fclose(dstFileID);                                                 % Close dst-file
 
-hold off;                                                                       % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
+hold off;                                                          % set the hold state to off so that new plots added to the axes clear existing plots and reset all axes properties.
 
 end
 
